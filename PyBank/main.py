35043adfb4973d6bd0d653eb1beb036f09630c8c
Martinezj93 <martinezjonathan225@gmail.com>
2023-01-09 01:10:58 +0800

    Task Completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,24 +5,24 @@
 import os
 import csv
 
+# Initital statement:
+            
+header = "Financial Analysis"
+print(header)
+
+separator = "--------------------------------------------------"
+print (separator)
+
 # Read and open CSV file:
 
 csvpath = os.path.join("Resources","budget_data.csv")
 with open(csvpath,"r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     
-    # Remove headers:
+    # Headers:
 
     hearder_line = next(csvreader)
     
-    # Initital statement:
-            
-    header = "Financial Analysis"
-    print(header)
-
-    separator = "--------------------------------------------------"
-    print (separator)
-
     # Calculation of total months and total profit and loss:
     
     months = []
@@ -32,9 +32,9 @@ with open(csvpath,"r") as csvfile:
         months.append(row[0])
         profit_and_loss.append(int(row[1]))
     
-    total_months = (f'Total Months: {len(months)}')
+    total_months = f'Total Months: {len(months)}'
     print(total_months)
-    total_amount = (f'Total: ${sum(profit_and_loss)}')
+    total_amount = f'Total: ${sum(profit_and_loss)}'
     print(total_amount)
 
     # Calculation of average change of profit and loss:
@@ -45,16 +45,17 @@ with open(csvpath,"r") as csvfile:
        change.append(int(profit_and_loss[i + 1] - profit_and_loss[i]))
            
     change_formula = sum(change) / (len(change)-1)
-    average_change = ("Average Change: $" + "%.2f" % change_formula)
+    average_change = "Average Change: $" + "%.2f" % change_formula
     print(average_change)
 
     # Calculation of the greatest increase and greatest decrease in profits:
 
-    greatest_increase = (f'Greatest Increase in Profits: {months[int(change.index(max(change)))]} (${str(max(change))})')
+    greatest_increase = f'Greatest Increase in Profits: {months[int(change.index(max(change)))]} (${str(max(change))})'
     print(greatest_increase)
-    greatest_decrease = (f'Greatest Decrease in Profits: {months[int(change.index(min(change)))]} (${str(min(change))})')
+    greatest_decrease = f'Greatest Decrease in Profits: {months[int(change.index(min(change)))]} (${str(min(change))})'
     print(greatest_decrease)
-    # Export results to "Analysis" text file:
+
+# Export results to "Analysis" text file:
 
 output_path = os.path.join("Analysis","Analysis.txt")
 with open (output_path,"w") as output_file:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,27 +5,83 @@
 import os
 import csv
 
-# Read and open CSV file:
+# Initital statement:
+            
+header = "Election Results"
+print(header)
+
+separator = "--------------------------------------------------"
+print (separator)
+
+# Identify location of the output file:
+
+output_path = os.path.join("Analysis","Analysis.txt")
+
+# Identify location, read and open CSV file:
 
 csvpath = os.path.join("Resources","election_data.csv")
 with open(csvpath,"r") as csvfile:
-    csvreader = csv.reader(csvfile,delimiter=",")
+    csvreader =  csv.reader(csvfile, delimiter=",")
 
-# Remove headers:
+    # Headers:
 
     hearder_line = next(csvreader)
-    
-    # Initital statement:
-            
-    print("Election Results")
-    print("--------------------------------------------------")
 
-    # Calculation of total months and total profit and loss:
-    
-    votes = []
-        
+    # Identify Variables:
+
+    total_votes = 0
+    candidates = []
+    candidate_votes = {}
+    winner = "x"
+    winner_votes = 0
+
+    # Read through te file and identify total number of votes and list of candidates who received votes:
+
     for row in csvreader:
-        votes.append(row[0])
+        total_votes = total_votes + 1
+        candidate_name = row[2]
+
+        if candidate_name not in candidates:
+           candidates.append(candidate_name)
+           candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
+
+    total_votes_result = f'Total Votes: {total_votes}'
+
+# Open the output file to start printing and exporting the results:
+
+with open (output_path,"w") as output_file:
     
-    total_votes = len(votes)
-    print(f'Total Months: {votes}')
+    print(total_votes_result)
+    print(separator)
+    output_file.write(str(header) + '\n')
+    output_file.write(str(separator) + '\n')
+    output_file.write(str(total_votes_result) + '\n')
+    output_file.write(str(separator) + '\n')
+
+    # Calculate percetage of votes and total number of votes per candidate:
+     
+    for candidates in candidate_votes:
+        votes = candidate_votes[candidates]
+        percentage = votes / total_votes * 100
+        results = f'{candidates}: {percentage: .3f}% ({votes})'
+        
+        print(results)
+        output_file.write(str(results) + '\n')
+
+    # Identify the winner:
+
+        if votes > winner_votes:
+            winner_votes = votes
+            winner = candidates
+        
+    print (separator)
+    output_file.write(str(separator) + '\n')
+
+    winner_name = f'Winner: {winner}'
+    
+    print (winner_name)
+    print (separator)
+    output_file.write(str(winner_name) + '\n')
+    output_file.write(str(separator) + '\n')
