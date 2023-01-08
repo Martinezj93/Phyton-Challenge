3debf1e110daeef455976d0946ce28221602cf66
Martinezj93 <martinezjonathan225@gmail.com>
2023-01-08 21:51:20 +0800

    Task Completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@
 
 import os
 import csv
+import string
 
 # Read and open CSV file:
 
@@ -11,14 +12,17 @@ csvpath = os.path.join("Resources","budget_data.csv")
 with open(csvpath,"r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
     
-    # Remove colum headers:
+    # Remove headers:
 
     hearder_line = next(csvreader)
     
     # Initital statement:
             
-    print("Financial Analysis")
-    print("--------------------------------------------------")
+    header = "Financial Analysis"
+    print(header)
+
+    separator = "--------------------------------------------------"
+    print (separator)
 
     # Calculation of total months and total profit and loss:
     
@@ -29,11 +33,10 @@ with open(csvpath,"r") as csvfile:
         months.append(row[0])
         profit_and_loss.append(int(row[1]))
     
-    total_months = len(months)
-    print(f'Total Months: {total_months}')
-
-    total_amount = sum(profit_and_loss)
-    print(f'Total: ${total_amount}')
+    total_months = (f'Total Months: {len(months)}')
+    print(total_months)
+    total_amount = (f'Total: ${sum(profit_and_loss)}')
+    print(total_amount)
 
     # Calculation of average change of profit and loss:
     
@@ -42,13 +45,24 @@ with open(csvpath,"r") as csvfile:
     for i in range(len(profit_and_loss)-1):
        change.append(int(profit_and_loss[i + 1] - profit_and_loss[i]))
            
-    average_change = sum(change) / (len(change)-1)
-    print("Average Change: $" + "%.2f" % average_change)
+    change_formula = sum(change) / (len(change)-1)
+    average_change = ("Average Change: $" + "%.2f" % change_formula)
+    print(average_change)
 
     # Calculation of the greatest increase and greatest decrease in profits:
 
-    greatest_increase = max(change)
-    print(f'Greatest Increase in Profits: {months[int(change.index(greatest_increase))]} (${str(greatest_increase)})')
+    greatest_increase = (f'Greatest Increase in Profits: {months[int(change.index(max(change)))]} (${str(max(change))})')
+    print(greatest_increase)
+    greatest_decrease = (f'Greatest Decrease in Profits: {months[int(change.index(min(change)))]} (${str(min(change))})')
+    print(greatest_decrease)
+    # Export results to "Analysis" text file:
 
-    greatest_decrease = min(change)
-    print(f'Greatest Decrease in Profits: {months[int(change.index(greatest_decrease))]} (${str(greatest_decrease)})')
+output_path = os.path.join("Analysis","Analysis.txt")
+with open (output_path,"w") as output_file:
+    output_file.writelines(str(header) + '\n')
+    output_file.writelines(str(separator) + '\n')
+    output_file.writelines(str(total_months) + '\n')
+    output_file.writelines(str(total_amount) + '\n')
+    output_file.writelines(str(average_change) + '\n')
+    output_file.writelines(str(greatest_increase) + '\n')
+    output_file.writelines(str(greatest_decrease) + '\n')
